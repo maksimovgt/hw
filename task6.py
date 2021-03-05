89951ccb8d994385cc892e413600778300a6834b
maksimovgt <gt.maksimov@gmail.com>
2021-03-05 23:32:16 +0900

    исправил задание 5 и 6

--- task6.py
+++ task6.py
@@ -1,8 +1,9 @@
-a = int(input('1 день кол-во км: '))
-b = int(input('общий результат км: '))
-d = 0
+# a = int(input('1 день кол-во км: '))
+# b = int(input('общий результат км: '))
+# d = 0
+#
+# while a < b:
+#     a = a + (a * 0.1)
+#     d = d + 1
+# print(d)
 
-while a < b:
-    a = a + (a * 0.1)
-    d = d + 1
-print(d)
