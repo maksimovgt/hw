708d5fb2bb3cda13300dea8c9ca36217f1cf5f48
maksimovgt <gt.maksimov@gmail.com>
2021-03-05 23:46:21 +0900

    исправил задание 6

--- task6.py
+++ task6.py
@@ -7,3 +7,21 @@
 #     d = d + 1
 # print(d)
 
+while True:
+    days = 1
+    start_km = float(input("Начальный результат - "))
+    last_km = float(input("Финальный результат - "))
+    if start_km <= 0 or last_km < 0:
+        print('Результаты должны быть больше нуля, стартовое значение не равно нулю')
+    else:
+        while start_km < last_km:
+            start_km *= 1.1
+            days += 1
+        print(f"Спортсмен добьется результата за {days} дней")
+
+# def km(res_min, res_max, days):
+#     if res_min > res_max:
+#         return days
+#     else:
+#         return km(res_min * 1.1, res_max, days + 1)
+# print(km(int(input('Введите начальный результат'), int(input('Введите конечный результат')))))
