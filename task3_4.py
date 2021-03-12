06b1396df7f05cf49b0301ef4683647d229cec08
maksimovgt <gt.maksimov@gmail.com>
2021-03-12 22:48:05 +0900

    сделал задание 4

---
+++ task3_4.py
@@ -0,0 +1,10 @@
+def my_func(x, y):
+    y: int
+    x: int
+    if x < 0 or y > 0:
+        return print('Ошибка, не выполнены условия')
+    else:
+        return print(x ** y)
+
+
+my_func(9, -2)
