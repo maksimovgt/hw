a14ccb809e80f028ad78aa60c9850c0bc25b0956
maksimovgt <gt.maksimov@gmail.com>
2021-03-15 19:48:18 +0900

    на проверку задание 6

---
+++ task4_6.py
@@ -0,0 +1,14 @@
+from itertools import count,cycle
+
+for i in count(int(input('Введите число, для выхода введите q '))):
+    print(i, end = '')
+    quit = input()
+    if quit == 'q':
+        break
+
+my_list = input('Введите символы через пробел, для выхода введите q! - ').split()
+i = cycle(my_list)
+quit = None
+while quit != 'q':
+    print(next(i), end='')
+    quit = input()
