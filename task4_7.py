e53d8525a9c26881d53882489b968ac5574e8b9f
maksimovgt <gt.maksimov@gmail.com>
2021-03-15 20:33:00 +0900

    на проверку задание 7

---
+++ task4_7.py
@@ -0,0 +1,11 @@
+def factorial(num):
+    fac_num = 1
+    if num == 0:
+        yield f'{num}! = 1'
+    for i in range(1, num + 1):
+        fac_num *= i
+        yield f'{i}! = {fac_num}'
+
+
+for el in factorial(int(input('Факториал числа - '))):
+    print(el)
