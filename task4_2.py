296c39ee65c2d5fa741c1b92ef96b1b698a11f6d
maksimovgt <gt.maksimov@gmail.com>
2021-03-15 18:32:47 +0900

    на проверку задание 2

--- task4_2.py
+++ task4_2.py
@@ -1,3 +1,3 @@
-a = [11, 55, 33, 54, 24, 1, 3, 99]
-new_list = [a[el] for el in range(0, len(a)) if a[el] > a[el - 1]]
+a = [11, 55, 33, 54, 24, 1, 3, 99, 100, 28, 54]
+new_list = [a[el] for el in range(1, len(a)) if a[el] > a[el - 1]]
 print(new_list)
